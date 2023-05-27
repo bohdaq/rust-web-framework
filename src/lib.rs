ee742448cbf296e26cae230d4694487aac0583d9
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-05-27 19:57:54 +0300

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -28,65 +28,22 @@ pub mod controller;
 pub mod json;
 
 
-use crate::entry_point::{bootstrap, get_ip_port_thread_count, set_default_values};
 use crate::server::Server;
-use crate::thread_pool::ThreadPool;
-use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpListener};
-use crate::log::Log;
-use crate::symbol::SYMBOL;
+use crate::app::App;
+use crate::core::New;
 
 pub fn start_server() {
 
-    let info = Log::info("Rust Web Server");
-    println!("{}", info);
-    let usage = Log::usage_information();
-    println!("{}", usage);
-
-
-    println!("RWS Configuration Start: \n");
-    set_default_values();
-    bootstrap();
-    println!("RWS Configuration End\n\n");
-
-
-    let (ip, port, thread_count) = get_ip_port_thread_count();
-    create_tcp_listener_with_thread_pool(ip.as_str(), port, thread_count);
-}
-
-pub fn create_tcp_listener_with_thread_pool(ip: &str, port: i32, thread_count: i32) {
-    let bind_addr = [ip, SYMBOL.colon, port.to_string().as_str()].join(SYMBOL.empty_string);
-    println!("Setting up server at protocol: http, ip {}, port {}", ip, port);
-
-    let boxed_listener = TcpListener::bind(&bind_addr);
-    if boxed_listener.is_err() {
-        eprintln!("unable to set up TCP listener: {}", boxed_listener.err().unwrap());
-    } else {
-        let listener = boxed_listener.unwrap();
-        let pool = ThreadPool::new(thread_count as usize);
-
-        let server_url_thread_count = Log::server_url_thread_count("http", &bind_addr, thread_count);
-        println!("{}", server_url_thread_count);
-
-        for boxed_stream in listener.incoming() {
-            if boxed_stream.is_err() {
-                eprintln!("unable to get TCP stream: {}", boxed_stream.err().unwrap());
-            } else {
-                let stream = boxed_stream.unwrap();
-
-                pool.execute(move || {
-
-                    let mut peer_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0,0,0,0)), 0);
-                    let boxed_peer_addr = stream.peer_addr();
-                    if boxed_peer_addr.is_ok() {
-                        peer_addr = boxed_peer_addr.unwrap()
-                    } else {
-                        eprintln!("\nunable to read peer addr");
-                    }
-
-                    Server::process_request(stream, peer_addr);
-                });
-            }
-        }
+    let new_server = Server::setup();
+    if new_server.is_err() {
+        eprintln!("{}", new_server.as_ref().err().unwrap());
     }
 
+
+    let (listener, pool) = new_server.unwrap();
+    let app = App::new();
+
+
+    Server::run(listener, pool, app);
 }
+
